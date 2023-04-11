5f0c991826c918abe8842debd3cbd7d6f42422bb
FarahAlhalimi <engfarahhalimy@gmail.com>
2023-04-11 13:48:57 +0300

    request changes closes #6

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,4 +1,4 @@
-require('env2')('.env');
+require('dotenv').config();
 const { Pool } = require('pg');
 const DATABASE_URL = process.env.DATABASE_URL;
 
--- server/database/queries/post.js
+++ server/database/queries/post.js
@@ -1,29 +1,39 @@
 const connection=require('../config/connection');
 
 
-const postData=(user,task,project )=>{
+const postUserData=(user)=>{
      
-    let {name,email,password} = user;
+    const {name,email,password} = user;
   const userSql = {
     text: "INSERT INTO users(name,email,password) VALUES($1, $2,$3) returning name,email,password",
     values: [name,email,password],
   };
 
-  
-  let {title,desc,deadline} = task;
-  const taskSql = {
-    text: "INSERT INTO tasks(title,desc,deadline) VALUES($1, $2,$3,$4) returning title,desc,deadline",
-    values: [title,desc,deadline],
-  };
 
-
-  let {pname,pdesc} = project;
-  const projectSql = {
-    text: "INSERT INTO tasks(pname,pdesc) VALUES($1, $2) returning pname,pdesc",
-    values: [name,desc],
-  };
-
-  return connection.query(userSql , taskSql, projectSql);
+  return connection.query(userSql);
 };
 
-module.exports = postData;
+
+const postTaskData=(task)=>{
+const {title,desc,deadline} = task;
+const taskSql = {
+  text: "INSERT INTO tasks(title,desc,deadline) VALUES($1, $2,$3,$4) returning title,desc,deadline",
+  values: [title,desc,deadline],
+};
+
+  return connection.query(taskSql);
+};
+
+const postProjectData=(task)=>{
+    
+   const {pname,pdesc} = project;
+const projectSql = {
+  text: "INSERT INTO tasks(pname,pdesc) VALUES($1, $2) returning pname,pdesc",
+  values: [pname,pdesc],
+}; 
+      return connection.query(taskSql);
+};
+
+
+
+module.exports = {postUserData,postTaskData,postProjectData};
